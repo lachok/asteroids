8784db44bd4afd7fd41de7c813d3b38197ce5e37
lachok <lachok@users.noreply.github.com>
2016-06-06 21:41:45 +0100

    Fix name

--- asteroids-renderer/src/Bullet.js
+++ asteroids-renderer/src/Bullet.js
@@ -2,7 +2,7 @@ import { fabric } from 'fabric'
 import * as shapes from './shapes'
 import * as utils from './utils'
 
-export default function Explosion({x, y}, canvas) {
+export default function Bullet({x, y}, canvas) {
     var line = new fabric.Line([0, 0, 1, 1], {
         left: x,
         top: y,
